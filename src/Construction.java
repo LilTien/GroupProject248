da44d03ad621ae18785fbd55a8c3bc8730f52153
Alif <ahmadalif1108@gmail.com>
2024-06-10 22:06:42 +0800

    ikmal hensem

---
+++ src/Construction.java
@@ -0,0 +1,108 @@
+import java.util.Date;
+
+public class Construction {
+
+    private String projectID;
+    private String projectCategory;
+    private String projectLocation;
+    private Date startDate;
+    private int estimatedTime;
+    private Date endDate;
+    private double budget;
+    private String projectStatus;
+    private String progressStatus;
+
+    //contructor
+    //default
+    Construction(){
+        projectID = null;
+        projectCategory = null;
+        projectLocation = null ;
+        startDate = null;
+        estimatedTime =0;
+        endDate = null;
+        budget = 0.00;
+        projectStatus =null;
+        progressStatus = null;
+    }
+    //normal
+    Construction(String pi, String pc, String pl, Date sd, int et, Date ed, double b, String ps, String pros){
+        projectID = pi;
+        projectCategory = pc;
+        projectLocation = pl ;
+        startDate = sd;
+        estimatedTime = et;
+        endDate = ed;
+        budget = b;
+        projectStatus =ps;
+        progressStatus = pros;
+    }
+
+
+    //getter method
+    public String getProjectID(){
+        return projectID;
+    }
+    public String getProjectCategory(){
+        return projectCategory;
+    }
+    public String getProjectLocation(){
+        return projectLocation;
+    }
+    public Date getStartDate(){
+        return startDate;
+    }
+    public int getEstimatedTime(){
+        return estimatedTime;
+    }
+    public Date getEndDate(){
+        return endDate;
+    }
+    public double getBudget(){
+        return budget;
+    }
+    public String getProjectStatus(){
+        return projectStatus;
+    }
+    public String getProgressStatus(){
+        return progressStatus;
+    }
+
+    //setter method
+    public void setProjectID(String pi){
+        projectID = pi;
+    }
+    public void setProjectCategory(String pc){
+        projectCategory = pc;
+    }
+    public void setProjectLocation(String pl){
+        projectLocation = pl;
+    }
+    public void setStartDate(Date sd){
+        startDate = sd;
+    }
+    public void setEstimatedTime(int e){
+        estimatedTime = e;
+    }
+    public void setEndDate(Date ed){
+        endDate = ed;
+    }
+    public void setBudget(double b){
+        budget = b;
+    }
+    public void setProjectStatus(String ps){
+        projectStatus = ps;
+    }
+    public void setProgressStatus(String pros){
+        progressStatus = pros;
+    }
+
+    //print method
+    public String toString(){
+        return "\nProject ID: " + projectID + "\nProject Category: " +
+                projectCategory + "\nProject Location: " +projectLocation +
+                "\nProject Start Date: " +startDate + "\n Estimated time: " +estimatedTime +
+                "\nProject End Date: " +endDate + "\nBudget: " +
+                budget + "\nProject Status: " +projectStatus + "\nProgress Status : " + projectStatus;
+    }
+}
